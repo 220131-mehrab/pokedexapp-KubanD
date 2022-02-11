38487f129806b732b5ac7baba9903e7d5a3e2ae5
KubanD936 <kuban627@revature.net>
2022-02-11 14:19:20 -0500

    First Commit

--- src/main/java/com/revature/pokedex/App.java
+++ src/main/java/com/revature/pokedex/App.java
@@ -1,9 +1,14 @@
 package com.revature.pokedex;
 
+import org.apache.catalina.startup.Tomcat;
+
 public class App {
     public static void main(String[] args) {
         DexRepository dexRepository = new DexRepository("pokedex.csv");
         DexService dexService = new DexService(dexRepository);
         DexServer server = new DexServer(dexService);
+
+        Tomcat server = new Tomcat();
+        server.setBaseDir(System.getProperty("java.io.tmpdir"));
     }
 }
